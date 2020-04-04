ac3b7db3b36d5b188e1ded1fa7fa46717572b26b
Haervaerk <45241565+Haervaerk@users.noreply.github.com>
2020-04-04 20:49:12 +0200

    Created "easy" ai

--- src/pack1/Main.java
+++ src/pack1/Main.java
@@ -3,6 +3,5 @@ public class Main{
     public static void main(String[] args) {
         new View(550,350);
         new ImageLoader();
-
     }
 }
--- src/pack1/SinglePlayerFrame.java
+++ src/pack1/SinglePlayerFrame.java
@@ -2,18 +2,18 @@ package src.pack1;
 
 import java.awt.Dimension;
 import java.awt.event.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
 public class SinglePlayerFrame extends JFrame implements GameFrame{
     private JButton buttons[] = new JButton[9];
     private int state[] = new int[9];
-    private int player = 1;
     private int totalMoves = 0;
-    private boolean winner = false;
     private int aIMode = 1;
     
     public SinglePlayerFrame(int width, int height){
@@ -36,18 +36,89 @@ public class SinglePlayerFrame extends JFrame implements GameFrame{
         if (state[i] != 0) return;
         else{
             state[i] = 1;
+            if (checkWin() != 0) {
+                //Create popupbox declaring winner
+                JOptionPane.showMessageDialog(this, "The Winner was: " + getWinnerFormat(checkWin()));
+                reset();
+                repaint();
+                validate();
+                return;
+            }
+            else if (totalMoves >= 9) {
+                //Create popupbox saying no one won
+                JOptionPane.showMessageDialog(this, "There was no winner");
+                reset();
+                repaint();
+                validate();
+                return;
+            }
             moveAI();
+            if (checkWin() != 0) {
+                //Create popupbox declaring winner
+                JOptionPane.showMessageDialog(this, "The Winner was: " + getWinnerFormat(checkWin()));
+                //reset();
+                repaint();
+                validate();
+            }
+
         }
-
-
-
     };
     
-    private void moveAI() {
-        if(aIMode == 1){
-            
+    private void reset() {
+        state = new int[9];
+        totalMoves = 0;
+        repaint();
+        validate();
+    }
+
+    private String getWinnerFormat(int winner) {
+        if (winner == 1) return "YOU";
+        else return "..  well.... not you";
+    }
+    //return 0 if no winner, 1 for player1 or 2 for player2
+    private int checkWin() {
+        if (state[0] != 0 && state[0] == state[1] && state[0] == state[2]) {
+            return state[0];    
+        }
+        else if (state[3] != 0 && state[3] == state[4] && state[3] == state[5]) {
+            return state[3];     
+        }
+        else if (state[6] != 0 && state[6] == state[7] && state[6] == state[8]) {
+            return state[6];    
         }
 
+        else if (state[0] != 0 && state[0] == state[3] && state[0] == state[6]) {
+            return state[0];    
+        }
+        else if (state[1] != 0 && state[1] == state[4] && state[1] == state[7]) {
+            return state[1];     
+        }
+        else if (state[2] != 0 && state[2] == state[5] && state[2] == state[8]) {
+            return state[2];    
+        }
+        else if (state[0] != 0 && state[0] == state[4] && state[0] == state[8]) {
+            return state[0];     
+        }
+        else if (state[2] != 0 && state[2] == state[4] && state[2] == state[6]) {
+            return state[2];     
+        }
+        totalMoves += 1;
+        return 0;
+    }
+
+    private void moveAI() {
+        if(aIMode == 1){
+            boolean searching = true;
+            while(searching){
+                int num = ThreadLocalRandom.current().nextInt(0, 9);
+                if (state[num] == 0){
+                    state[num] = 2;
+                    searching = false;
+                }
+            }
+            repaint();
+            validate(); 
+        }
     }
 
     private void CreateButtons() {
